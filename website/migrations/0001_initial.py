5b23876596af58e7ade081af4cf0217a972822f9
DannyBoy <Danny@Boy.com>
2024-05-23 13:25:16 +0200

    website/migrations/0001_initial.py személyre szabva

--- website/migrations/0001_initial.py
+++ website/migrations/0001_initial.py
@@ -15,15 +15,10 @@ class Migration(migrations.Migration):
             name='Record',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('first_name', models.CharField(max_length=50)),
-                ('last_name', models.CharField(max_length=50)),
-                ('email', models.CharField(max_length=100)),
-                ('phone', models.CharField(max_length=15)),
-                ('address', models.CharField(max_length=100)),
-                ('city', models.CharField(max_length=50)),
-                ('state', models.CharField(max_length=50)),
-                ('zipcode', models.CharField(max_length=20)),
+                ('name', models.CharField(max_length=50)),
+                ('price', models.IntegerField()),
+                ('kiszereles', models.CharField(max_length=5)),
+                ('bevezetes_datuma', models.DateTimeField())
             ],
         ),
     ]
